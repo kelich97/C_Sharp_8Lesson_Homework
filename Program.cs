87c0f4ec49238810946b7fcb242cac2df3efc1ff
kelich97 <135359551+kelich97@users.noreply.github.com>
2023-07-11 02:28:14 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,21 +1,18 @@
-﻿//Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по 
-//убыванию элементы каждой строки двумерного массива.
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
+//которая будет находить строку с наименьшей суммой элементов.
 //Например, задан массив:
 //1 4 7 2
 //5 9 2 3
 //8 4 2 4
-//В итоге получается вот такой массив:
-//7 4 2 1
-//9 5 3 2
-//8 4 4 2
+//5 2 6 7
+//Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 
-
-int [,] CreateArray (int rows, int collumns, int minV, int maxV)
+int [,] CreateArray ()
 {
-    int [,] createdArray = new int [rows, collumns];
-    for (int i = 0; i < rows; i++)
-        for (int j = 0; j < collumns; j++)
-            createdArray[i, j] = new Random().Next(minV,maxV);
+    int [,] createdArray = new int [5,5];
+    for (int i = 0; i < 5; i++)
+        for (int j = 0; j < 5; j++)
+            createdArray[i, j] = new Random().Next(1,9);
     return createdArray;        
 }
 
@@ -32,46 +29,31 @@ void ShowArray (int [,] array)
     Console.WriteLine();   
 }
 
-Console.WriteLine("input rows");
-int rows = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("input collumns");
-int collumns = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("input min value");
-int min = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("input max value");
-int max = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine();
-
-int [,] ArraySort (int [,] array)
+void ShowArr (int [,] array)
 {
-    int temp = 0;
-    int count = 0; 
-    while(count <= array.GetLength(0))
+    int [] arr = new int [array.GetLength(0)];
+    int min = 100;
+    for (int i = 0; i < array.GetLength(0); i++)
     {    
-        for(int i = 0; i < array.GetLength(0); i++)
+        int sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-            for(int j = 1; j < array.GetLength(1); j++)
-            {
-                if(array[i,j] > array[i,j - 1])
-                {
-                    temp = array[i,j - 1];
-                    array[i,j - 1] = array[i,j];
-                    array[i,j] = temp;
-                }
-            }
+            sum = sum + array[i,j];
         }
-        count += 1;
-    }      
-    return array;
+        if(sum < min)
+        {
+            min = sum;
+        } 
+        Console.Write($"{arr[i] = sum} ");   
+    }
+    for (int i = 0; i < arr.Length; i++) 
+    if(arr[i] == min)
+        Console.WriteLine("строка ->" + (i + 1));
 }
 
-int [,] newArray = CreateArray(rows, collumns, min, max);
+int [,] newArray = CreateArray();
 ShowArray(newArray);
-
-int [,] arr = ArraySort(newArray);
-ShowArray(arr);
-
-
+ShowArr(newArray);
 
 
 
